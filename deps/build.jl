cdd5f1f1e476642f7355bd5dd6237410f76ee7cb
Josh Christie <joshua.christie@roames.com.au>
2017-08-28 11:46:44 +1000

    Windows build attempt 1

--- deps/build.jl
+++ deps/build.jl
@@ -6,6 +6,12 @@ deps = [
     libpng = library_dependency("libpng", aliases = ["libpng","libpng-1.5.14","libpng15","libpng12.so.0","libpng12"])
 ]
 
-# TODO Add zlib
+provides(AptGet, "libpng12-0", libpng)
+
+if is_windows()
+    using WinRPM
+    provides(WinRPM.RPM, "zlib-devel", zlib, os = :Windows)
+    provides(WinRPM.RPM, "libpng-dev", libpng, os = :Windows)
+end
 
 @BinDeps.install Dict(:libpng => :libpng)
