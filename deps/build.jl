6db06f5a9a0743a1ac6952e4413aadd090dbc5c8
Josh Christie <joshua.christie@roames.com.au>
2017-08-28 11:56:49 +1000

    Add zlib

--- deps/build.jl
+++ deps/build.jl
@@ -4,6 +4,7 @@ using BinDeps
 
 deps = [
     libpng = library_dependency("libpng", aliases = ["libpng","libpng-1.5.14","libpng15","libpng12.so.0","libpng12"])
+    zlib = library_dependency("zlib", aliases = ["libzlib","zlib1"], os = :Windows)
 ]
 
 provides(AptGet, "libpng12-0", libpng)
