37ae9c93d0751483a32675218afaba12ffd7ed0a
Mandeep <mandeep.bahal@roames.com.au>
2019-11-12 03:24:39 +1000

    PIPE-167 Fix build scripts
    * Add Random.jl package for tests
    * Try to fix appveyor
    * remove osx support
    * Fix windows build
    * Remove windows build support

--- src/LibPNG.jl
+++ src/LibPNG.jl
@@ -11,6 +11,7 @@ export readimage, writeimage
 const depsfile = joinpath(dirname(@__FILE__), "..", "deps", "deps.jl")
 if isfile(depsfile)
     include(depsfile)
+    check_deps()
 else
     error("LibPNG not properly installed. Please run Pkg.build(\"LibPNG\") then restart Julia.")
 end
